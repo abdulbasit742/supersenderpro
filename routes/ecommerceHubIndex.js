ae068b1bd5a08b73f8dd4742d0514c880fd7dec0
ABDUL BASIT <absh5506@gmail.com>
2026-06-30 16:28:28 +0500

    chore(ecommerce-hub): mount pipeline orchestrator routes in aggregator index

--- routes/ecommerceHubIndex.js
+++ routes/ecommerceHubIndex.js
@@ -33,6 +33,7 @@ mount('./ecommerceHubBatch5Routes');
 mount('./ecommerceHubBatch6Routes');
 mount('./ecommerceHubBatch7Routes');
 mount('./ecommerceHubBatch8Routes');
+mount('./ecommerceHubPipelineRoutes');
 mount('./ecommerceHubAdminRoutes');
 
 router.get('/_modules', function (req, res) {
